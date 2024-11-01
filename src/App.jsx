a89d4fea57f61088404c38574929f967037e18ac
MihailPetkov96 <mpetkov228@gmail.com>
2024-11-01 23:50:19 +0200

    Complete exercise 6.22

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
-import { useQuery } from '@tanstack/react-query';
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
 
-import { getAnecdotes } from './requests';
+import { getAnecdotes, updateAnecdote } from './requests';
 
 import AnecdoteForm from './components/AnecdoteForm';
 import Notification from './components/Notification';
 
 const App = () => {
+  const queryClient = useQueryClient();
+
+  const updateAnecdoteMutation = useMutation({
+    mutationFn: updateAnecdote,
+    onSuccess: () => {
+      queryClient.invalidateQueries('anecdotes');
+    }
+  });
 
   const handleVote = (anecdote) => {
-    console.log('vote');
+    const updatedAnecdote = {
+      ...anecdote,
+      votes: anecdote.votes + 1
+    };
+    updateAnecdoteMutation.mutate(updatedAnecdote);
   };
 
   const result = useQuery({
